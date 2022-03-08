b43937a0cdc03f731c904f9a4293be28ee214080
zydrunaszujus <89936656+zydrunaszujus@users.noreply.github.com>
2022-03-08 20:57:38 +0200

    Zydrius pakeitimai

--- Kovo8/Kovo8/Controllers/TulpeModelsController.cs
+++ Kovo8/Kovo8/Controllers/TulpeModelsController.cs
@@ -77,6 +77,17 @@ namespace Kovo8.Controllers
 
             return View(tulpeModel);
         }
+        public async Task<IActionResult> ZydriusTulpe()
+        {
+            var tulpeModel = await _context.Tulpes
+                .FirstOrDefaultAsync(m => m.Id == 2);
+            if (tulpeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(tulpeModel);
+        }
 
         // GET: TulpeModels/Create
         public IActionResult Create()

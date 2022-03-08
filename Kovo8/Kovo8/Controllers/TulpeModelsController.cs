919f0bed23625e7abb1fe48f69c548e0eb49478d
jonaspetraska7 <60759096+jonaspetraska7@users.noreply.github.com>
2022-03-08 19:16:39 +0200

    Controleris ir meniu

---
+++ Kovo8/Kovo8/Controllers/TulpeModelsController.cs
@@ -0,0 +1,153 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Kovo8.Models;
+
+namespace Kovo8.Controllers
+{
+    public class TulpeModelsController : Controller
+    {
+        private readonly MainDbContext _context;
+
+        public TulpeModelsController(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TulpeModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tulpes.ToListAsync());
+        }
+
+        // GET: TulpeModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tulpeModel = await _context.Tulpes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tulpeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(tulpeModel);
+        }
+
+        // GET: TulpeModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TulpeModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Vardas,Gele,Balsai")] TulpeModel tulpeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tulpeModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tulpeModel);
+        }
+
+        // GET: TulpeModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tulpeModel = await _context.Tulpes.FindAsync(id);
+            if (tulpeModel == null)
+            {
+                return NotFound();
+            }
+            return View(tulpeModel);
+        }
+
+        // POST: TulpeModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Vardas,Gele,Balsai")] TulpeModel tulpeModel)
+        {
+            if (id != tulpeModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tulpeModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TulpeModelExists(tulpeModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tulpeModel);
+        }
+
+        // GET: TulpeModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tulpeModel = await _context.Tulpes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tulpeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(tulpeModel);
+        }
+
+        // POST: TulpeModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tulpeModel = await _context.Tulpes.FindAsync(id);
+            _context.Tulpes.Remove(tulpeModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TulpeModelExists(int id)
+        {
+            return _context.Tulpes.Any(e => e.Id == id);
+        }
+    }
+}

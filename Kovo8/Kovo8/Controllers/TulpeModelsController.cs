26c2eec4c55af1b660c43d07dfa477b625e46063
ArnasStanislovaitis <89936719+ArnasStanislovaitis@users.noreply.github.com>
2022-03-08 21:42:39 +0200

    Arno pakeitimai

--- Kovo8/Kovo8/Controllers/TulpeModelsController.cs
+++ Kovo8/Kovo8/Controllers/TulpeModelsController.cs
@@ -100,6 +100,17 @@ namespace Kovo8.Controllers
 
             return View(tulpeModel);
         }
+        public async Task<IActionResult> ArnoTulpe()
+        {
+            var tulpeModel = await _context.Tulpes
+                .FirstOrDefaultAsync(m => m.Id == 4);
+            if (tulpeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(tulpeModel);
+        }
 
         // GET: TulpeModels/Create
         public IActionResult Create()

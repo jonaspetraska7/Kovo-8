387a0d5e4af08c0f503446ff4f37e36b3131584b
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2022-03-08 21:21:04 +0200

    commit

--- Kovo8/Kovo8/Controllers/TulpeModelsController.cs
+++ Kovo8/Kovo8/Controllers/TulpeModelsController.cs
@@ -77,7 +77,17 @@ namespace Kovo8.Controllers
 
             return View(tulpeModel);
         }
+        public async Task<IActionResult> VilkoTulpe()
+        {
+            var tulpeModel = await _context.Tulpes
+                .FirstOrDefaultAsync(m => m.Id == 5);
+            if (tulpeModel == null)
+            {
+                return NotFound();
+            }
 
+            return View(tulpeModel);
+        }
         // GET: TulpeModels/Create
         public IActionResult Create()
         {
